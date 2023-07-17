208d05343ebf7148462c3be4319f829546857981
Geodor Ruales <geodor.ruales@telus.com>
2023-07-17 20:44:59 +0800

    feat: add destroy

--- src/classes/CTerraform.js
+++ src/classes/CTerraform.js
@@ -20,6 +20,12 @@ class Terraform {
       encoding: "utf-8",
     });
   };
+
+  apply = (relativePath) => {
+    return execSync(`terraform -chdir=${relativePath} apply -destroy -auto-approve`, {
+      encoding: "utf-8",
+    });
+  };
 }
 
 module.exports = { Terraform };
