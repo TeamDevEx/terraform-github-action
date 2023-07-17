91523ad338f0456e9df6637b7f7f762ec6870d8c
Geodor Ruales <geodor.ruales@telus.com>
2023-07-17 19:33:52 +0800

    refactor: relocate getFiles function

---
+++ src/util/getFiles.js
@@ -0,0 +1,20 @@
+const { promisify } = require("util");
+const readdir = promisify(fs.readdir);
+const stat = promisify(fs.stat);
+
+async function* getFiles(directory = ".") {
+  for (const file of await readdir(directory)) {
+    const fullPath = path.join(directory, file);
+    const stats = await stat(fullPath);
+
+    if (stats.isDirectory()) {
+      yield* getFiles(fullPath);
+    }
+
+    if (stats.isFile()) {
+      yield fullPath;
+    }
+  }
+}
+
+module.exports = { getFiles };
