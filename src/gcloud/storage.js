74a59a484bdc33cd2f9a9496510e85fbb7d2b42a
Geodor Ruales <geodor.ruales@telus.com>
2023-07-17 19:34:25 +0800

    refactor: import getFiles

--- src/gcloud/storage.js
+++ src/gcloud/storage.js
@@ -1,25 +1,7 @@
-const { promisify } = require("util");
 const fs = require("fs");
 const path = require("path");
 const { logger } = require("../util/logger");
-
-const readdir = promisify(fs.readdir);
-const stat = promisify(fs.stat);
-
-async function* getFiles(directory = ".") {
-  for (const file of await readdir(directory)) {
-    const fullPath = path.join(directory, file);
-    const stats = await stat(fullPath);
-
-    if (stats.isDirectory()) {
-      yield* getFiles(fullPath);
-    }
-
-    if (stats.isFile()) {
-      yield fullPath;
-    }
-  }
-}
+const { getFiles } = require("../util/getFiles");
 
 async function uploadDirectory(
   cloudStorageClient,
