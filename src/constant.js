823a1310b53a5c7b404234287e74f88d48962569
Geodor Ruales <geodor.ruales@telus.com>
2023-07-17 17:00:22 +0800

    refactor: put constant variables into one file

---
+++ src/constant.js
@@ -0,0 +1,7 @@
+const BUCKET_NAME = "terraform-config-states";
+const OLD_STATE_FOLDER = "old-state";
+
+const terraformDirPath = "terraform";
+const repoName = "sample-repo-to-use-gh-action";
+
+module.exports = { BUCKET_NAME, OLD_STATE_FOLDER, terraformDirPath, repoName };
