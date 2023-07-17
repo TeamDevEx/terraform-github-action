6347be9f822e2be6ba38b2d81b6fcbd89f229698
Geodor Ruales <geodor.ruales@telus.com>
2023-07-17 17:01:25 +0800

    refactor: relocate createResourcesProcess function

---
+++ src/proceses/createResourcesProcess.js
@@ -0,0 +1,59 @@
+const {
+  uploadDirectory,
+  doesBucketExist,
+  createBucket,
+  downloadFolder,
+  deleteDirectory,
+} = require("./gcloud/storage");
+const { isEmptyDir } = require("../util/fsProcesses");
+// const { getInput } = require("@actions/core");
+// const github = require("@actions/github");
+const fs = require("fs");
+const path = require("path");
+const { logger } = require("../util/logger");
+
+const createResourcesProcess = async (
+  cloudStorageClient,
+  terraformClient,
+  { repoName, terraformDirPath, bucketName, oldStateFolder }
+) => {
+  const isBucketExist = await doesBucketExist(bucketName);
+  if (!isBucketExist) await createBucket(cloudStorageClient, { bucketName });
+  if (!fs.existsSync(repoName)) fs.mkdirSync(repoName);
+  if (!fs.existsSync(oldStateFolder)) fs.mkdirSync(oldStateFolder);
+
+  await downloadFolder(cloudStorageClient, {
+    folderName: repoName,
+    bucketName,
+  });
+
+  fs.cpSync(terraformDirPath, repoName, { recursive: true });
+
+  const isOldStateEmpty = await isEmptyDir(oldStateFolder);
+  logger(`isOldStateEmpty: ${isOldStateEmpty}`);
+  const whatFolderToUse = isOldStateEmpty ? repoName : oldStateFolder;
+
+  // const absolutePathForTerraformProcesses = __dirname + "/" + whatFolderToUse;
+
+  await terraformClient.init(whatFolderToUse);
+  const planResponse = await terraformClient.plan(whatFolderToUse, {
+    autoApprove: true,
+  });
+
+  logger(planResponse);
+
+  const applyResponse = await terraformClient.apply(whatFolderToUse, {
+    autoApprove: true,
+  });
+
+  if (!isOldStateEmpty)
+    fs.cpSync(oldStateFolder, repoName, { recursive: true });
+
+  if (isBucketExist)
+    await deleteDirectory(bucketName, { bucketName, folderName: repoName });
+  await uploadDirectory(cloudStorageClient, { directoryPath: repoName });
+
+  logger(applyResponse);
+};
+
+module.exports = { createResourcesProcess };
